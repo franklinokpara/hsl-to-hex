17ecee4903024b466ac0f8c2a0ab3f72e1f137ef
macfrankiee <frankmaclin361@gmail.com>
2017-11-25 05:44:09 +0100

    4th commit

--- test/index.js
+++ test/index.js
@@ -29,67 +29,62 @@ test('hue', ({is, end}) => {
     const it = `
           Odeg should be red
         `
-    is(actual, expected, it)    
-    }
-    {
+    is(actual, expected, it)
+  }
+  {
     const expected = '#0000ff'
-    const actual = hsl (240, 100, 50)
+    const actual = hsl(240, 100, 50)
     const it = `
           240deg should be blue
         `
-      is(actual, expected, it)
-
-    }
-    {
-      const expected = '#00ffff'
-      const actual = hsl (180, 100, 50)
-      const it = `
+    is(actual, expected, it)
+  }
+  {
+    const expected = '#00ffff'
+    const actual = hsl(180, 100, 50)
+    const it = `
           180deg should be cyan
         `
-      is(actual, expected, it)
-    }
-    end()
+    is(actual, expected, it)
+  }
+  end()
 })
 
 test('degree overflow/underflow', ({is, end}) => {
-    {
-      const expected = hsl(1, 100, 50)
-      const actual = hsl (361, 100, 50)
-      const it = `
+  {
+    const expected = hsl(1, 100, 50)
+    const actual = hsl(361, 100, 50)
+    const it = `
           361deg should be the same as 1deg
         `
-      is(actual, expected, it)
-        
-    }
-    {
-        const expected = hsl(-1, 100, 50)
-        const actual = hsl (359, 100, 50)
-        const it = `
+    is(actual, expected, it)
+  }
+  {
+    const expected = hsl(-1, 100, 50)
+    const actual = hsl(359, 100, 50)
+    const it = `
           -1deg should be the same as 359deg
         `
-        is(actual, expected, it)
-    }
-    end()
- })
-
-
-
-  test('max constarint', ({is, end}) => {
-     {
-      const expected = hsl(0, 101, 50)
-      const actual = hsl(0, 100, 50)
-      const it = `
+    is(actual, expected, it)
+  }
+  end()
+})
+test('max constarint', ({is, end}) => {
+  {
+    const expected = hsl(0, 101, 50)
+    const actual = hsl(0, 100, 50)
+    const it = `
           101% should be the same as 100%
         `
-        is(actual, expected, it)
-    }
-   {
-     const expected = hsl(0, -1, 50)
-     const actual = hsl(0, 0, 50)
-     const it = `
+    is(actual, expected, it)
+  }
+  {
+    const expected = hsl(0, -1, 50)
+    const actual = hsl(0, 0, 50)
+    const it = `
           -1% should be the same
         `
-     is(actual, expected, it)
-   }
-    end()
-  })  
+    is(actual, expected, it)
+  }
+  end()
+})
